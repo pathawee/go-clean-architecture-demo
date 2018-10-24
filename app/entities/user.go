5a37584cfe3c9eb314e897136dd2e5c24503bb40
Ake <ake.service@gmail.com>
2018-10-24 15:15:05 +0700

    Rename models to entities

---
+++ app/entities/user.go
@@ -0,0 +1,8 @@
+package entities
+
+type User struct {
+	ID          int64  `json:"id"`
+	PhoneNumber string `json:"phone_number" validate:"required"`
+	Password    string `json:"-"`
+	Name        string `json:"name"`
+}
